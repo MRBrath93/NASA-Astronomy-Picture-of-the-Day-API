69b2c47da8df6fc9a5dfe1a86eeb36851871a13d
MRBrath93 <147531334+MRBrath93@users.noreply.github.com>
2024-04-08 18:56:49 +0200

    style

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,26 @@
 const url = "https://api.nasa.gov/planetary/apod?api_key=KHawM29oiGeTHWLrJIThRDWq0LJDVFK0zhX6PE4w&start_date=2024-04-02"
+const wrapperEl = document.querySelector(".wrapper");
 
 fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => renderApodImages(data))
     .catch(err => console.log("Ups noget gik galt....", err))
 
 
+function renderApodImages(images) {
+    images.forEach(image => {
+        const newArticle = document.createElement("article")
+        const newH2 = document.createElement("h2")
+        const newP = document.createElement("p")
+        const newImg = document.createElement("img")
 
-function inpendImg 
+        newH2.textContent = image.title
+        newP.textContent = image.explanation
+
+        newImg.src = image.url
+        newImg.alt = "ALT";
+
+        newArticle.append(newH2, newImg, newP)
+        wrapperEl.append(newArticle)
+    })
+}
