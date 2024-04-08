fe036aca3c646c1846b27cd8e39f126cd8987005
MRBrath93 <147531334+MRBrath93@users.noreply.github.com>
2024-04-08 15:09:27 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
-const url = "https://api.nasa.gov/planetary/apod?api_key=KHawM29oiGeTHWLrJIThRDWq0LJDVFK0zhX6PE4w"
+const url = "https://api.nasa.gov/planetary/apod?api_key=KHawM29oiGeTHWLrJIThRDWq0LJDVFK0zhX6PE4w&start_date=2024-04-02"
 
 fetch(url)
-.then
-.then
-.catch
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log("Ups noget gik galt....", err))
+
+
+
+function inpendImg 
